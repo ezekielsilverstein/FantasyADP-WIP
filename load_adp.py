84190207957e4ddedf0475b4804cb2bef5eb0186
ezekielsilverstein <ezekiel.m.silverstein@gmail.com>
2017-10-11 14:58:04 -0400

    Create tables for standard, ppr, and half-ppr scoring

--- load_adp.py
+++ load_adp.py
@@ -1,21 +1,28 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-from models import Player
+from models import PlayerStandard, PlayerPPR, PlayerHalfPPR
 from instantiate_session import session
+from argparse import ArgumentParser
 
-def grab_adp_rows():
+def grab_adp_rows(scoring):
     """
     Use Beautiful Soup to retrieve the rows of a player's ADP
     :return: rows
     """
-    url = 'https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php'
+    if scoring == 'ppr':
+        url = 'https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php'
+    elif scoring == 'standard':
+        url = 'https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'
+    elif scoring == 'half-ppr':
+        url = 'https://www.fantasypros.com/nfl/rankings/half-point-ppr-cheatsheets.php'
+
     response = requests.get(url)
     soup = BeautifulSoup(response.text)
     rows = soup.findAll('tr', attrs={'class': re.compile('mpb-player*')})
     return rows
 
-def load_into_adp(rows):
+def load_into_adp(rows, scoring):
     """
     Create a 'Player' object for each row and add it to the session
     Commit the entire session or rollback
@@ -39,7 +46,15 @@ def load_into_adp(rows):
             position_rank = data[3][2:]
         if data[9] == "":
             data[9] = 1000
-        p = Player(
+
+        if scoring == 'standard':
+            playermodel = PlayerStandard
+        elif scoring == 'ppr':
+            playermodel = PlayerPPR
+        elif scoring == 'half-ppr':
+            playermodel = PlayerHalfPPR
+
+        p = playermodel(
             rank = data[0],
             name = name,
             team = team,
@@ -63,8 +78,10 @@ def load_into_adp(rows):
         return e
 
 def main():
-    rows = grab_adp_rows()
-    load_into_adp(rows)
+    scoring_methods = ['standard', 'ppr', 'half-ppr']
+    rows_dict = {s: grab_adp_rows(s) for s in scoring_methods}
+    for scoring, rows in rows_dict.iteritems():
+        load_into_adp(rows, scoring)
     return
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -3,8 +3,40 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Player(Base):
-    __tablename__ = 'players'
+class PlayerStandard(Base):
+    __tablename__ = 'players_standard'
+    id = Column(Integer, primary_key=True)
+    rank = Column(Integer)
+    name = Column(String)
+    team = Column(String)
+    posrank_str = Column(String)
+    bye = Column(String)
+    adp = Column(Float)
+    best = Column(Integer)
+    worst = Column(Integer)
+    avg = Column(Float)
+    stdev = Column(Float)
+    position = Column(String)
+    position_rank = Column(Integer)
+
+class PlayerPPR(Base):
+    __tablename__ = 'players_ppr'
+    id = Column(Integer, primary_key=True)
+    rank = Column(Integer)
+    name = Column(String)
+    team = Column(String)
+    posrank_str = Column(String)
+    bye = Column(String)
+    adp = Column(Float)
+    best = Column(Integer)
+    worst = Column(Integer)
+    avg = Column(Float)
+    stdev = Column(Float)
+    position = Column(String)
+    position_rank = Column(Integer)
+
+class PlayerHalfPPR(Base):
+    __tablename__ = 'players_half_ppr'
     id = Column(Integer, primary_key=True)
     rank = Column(Integer)
     name = Column(String)
