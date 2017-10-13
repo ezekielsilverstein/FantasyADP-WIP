1d588833998b2695ff0295036f0525513ae9b71d
ezekielsilverstein <ezekiel.m.silverstein@gmail.com>
2017-10-12 20:52:49 -0400

    Create `config.ini` file for psql username and password

--- instantiate_session.py
+++ instantiate_session.py
@@ -1,10 +1,16 @@
+import configparser
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
 
 from models import Base
 
-db_string = 'postgresql://ezekielsilverstein:Maccabee@localhost:5432/adp'
+config = configparser.ConfigParser()
+config.read('config.ini')
+username = config['DEFAULT']['username']
+password = config['DEFAULT']['password']
+
+db_string = 'postgresql://{}:{}@localhost:5432/redraft_adp'.format(username, password)
 db = sqlalchemy.create_engine(db_string, client_encoding='utf8')
 if not database_exists(db.url):
     create_database(db.url)
